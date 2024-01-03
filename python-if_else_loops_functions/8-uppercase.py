9ea22d644c9a4650e1f6b87548b7288aa56cc0f5
chapmanhunt4 <chapmanhunt4@gmail.com>
2024-01-03 14:12:24 -0800

    converts number to uppercase

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for char in str:
+        if 97 <= char <= 122:
+            result += chr(ord(char) - 32)
+        else:
+            result += char
+    print(result)
