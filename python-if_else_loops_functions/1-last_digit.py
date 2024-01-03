39995679530f58c6705640b89b55ef4249f6e27a
chapmanhunt4 <chapmanhunt4@gmail.com>
2024-01-03 11:24:08 -0800

    made variable to store last digit and used absolute value remainder 10 to find it

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-if str(number[:5]) > 5:
-    print("Last digit of", number, "is", number[:5], "and is greater than 5")
-elif str(number[:5]) == 0:
-    print("Last digit of", number, "is", number[:5], "and is 0")
+last_digit = abs(number) % 10
+if last_digit > 5:
+    print("Last digit of", number, "is", last_digit, "and is greater than 5")
+elif last_digit == 0:
+    print("Last digit of", number, "is", last_digit, "and is 0")
 else:
-    print("Last digit of", number, "is", number[:5], "and is less than 6 and not 0")
+    print("Last digit of", number, "is", last_digit, "and is less than 6 and not 0")
