09a55d71f4c5aae0d6da19e9c88dcee423eaf708
chapmanhunt4 <chapmanhunt4@gmail.com>
2024-01-03 14:52:19 -0800

    made extra if else statement to correct

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last_digit = abs(number) % 10
+if number < 0:
+    last_digit = -(abs(number) % 10)
+else:
+    last_digit = abs(number) % 10
+
 print("Last digit of", number, "is ", end="")
 
 if last_digit > 5:
