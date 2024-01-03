d71e19a2faf88ebc9461b00d84c488ddf1245ccd
chapmanhunt4 <chapmanhunt4@gmail.com>
2024-01-03 12:34:50 -0800

    took out loop, used splice to make sure each number is 2 digits

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-for i in range(0, 10):
-    print("0{}, ".format(i), end="")
-for i in range(10, 100):
-    print("{}, ".format(i), end="")
+for i in range(0, 100):
+    print("{:02}, ".format(i), end="")
