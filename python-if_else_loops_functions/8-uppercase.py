7da439fd5bc209bd37cc144a37ad10a3f4662e6c
chapmanhunt4 <chapmanhunt4@gmail.com>
2024-01-03 14:15:44 -0800

    changed from ascii to char in first if

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
     for char in str:
-        if 97 <= char <= 122:
+        if 'a' <= char <= 'z':
             result += chr(ord(char) - 32)
         else:
             result += char
