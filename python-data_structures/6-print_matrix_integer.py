256b700296de77293601bfafcfda5df15434d819
chapmanhunt4 <chapmanhunt4@gmail.com>
2024-01-06 13:12:15 -0800

    trying to handle empty matrix

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-        for row in matrix:
-            if len(row) == 0:
-                print(" ")
-            else:
-                for num in row:
-                    print("{:d}".format(num), end=" " if num != row[-1] else "\n")
+    for row in matrix:
+        if len(row) == 0:
+            print("")
+        else:
+            for num in row:
+                print("{:d}".format(num), end=" " if num != row[-1] else "\n")
