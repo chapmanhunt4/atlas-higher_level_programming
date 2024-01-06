b2f8a8e5398eceb9b0028af53ffd2fa88b97ddda
chapmanhunt4 <chapmanhunt4@gmail.com>
2024-01-06 12:28:57 -0800

    fixed print statement to remove extra spaces at end of each list

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,4 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for num in row:
-            print("{:d}".format(num), end=" ")
-        print()
+            print("{:d}".format(num), end=" " if num != row[-1] else "\n")
