d046a6bfb173ce9fd58db235b60a7ad3d248d647
chapmanhunt4 <chapmanhunt4@gmail.com>
2024-01-08 14:59:36 -0800

    changed it to be simple and correct

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    common = set()
-    for i in set_1:
-        for j in set_2:
-            if i == j:
-                common = i
+    common = set_1 & set_2
     return common
