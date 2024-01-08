776eadcca894ded625f0f8990ee2bd93ad197536
chapmanhunt4 <chapmanhunt4@gmail.com>
2024-01-08 12:39:26 -0800

    did it without using max function

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -2,5 +2,9 @@
 def max_integer(my_list=[]):
     if len(my_list) == 0:
         return None
-    else:
-        return max(my_list)
+
+    max_num = my_list[0]
+    for num in my_list[1:]:
+        if num > max_num:
+            max_num = num
+    return max_num
