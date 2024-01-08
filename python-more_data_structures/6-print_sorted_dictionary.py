d055227bae6dbcbbc15c79b606cadc236f4ddcdf
chapmanhunt4 <chapmanhunt4@gmail.com>
2024-01-08 15:28:55 -0800

    made script that returns a sorted dictionary

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    for i in sorted(a_dictionary):
-        return i
+    sorted_dict = dict(sorted(a_dictionary.items()))
+    return sorted_dict
