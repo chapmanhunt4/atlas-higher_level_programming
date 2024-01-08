802a26121a8e09a62e748fa0a9bedb2b7a1e2fc4
chapmanhunt4 <chapmanhunt4@gmail.com>
2024-01-08 15:34:08 -0800

    forgot to print this time, tried to return

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
     sorted_dict = dict(sorted(a_dictionary.items()))
-    return sorted_dict
+    for key, value in sorted_dict.items():
+        print("{}: {}".format(key, value))
