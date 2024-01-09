5dadba13fb00af3adb08364ec687aecb8de22943
chapmanhunt4 <chapmanhunt4@gmail.com>
2024-01-09 11:41:14 -0800

    fixed to adjust for pycode style

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -3,13 +3,15 @@ def roman_to_int(roman_string):
     if type(roman_string) != str or roman_string is None:
         return 0
 
-    roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    r_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
 
     result = 0
     for i in range(len(roman_string)):
-        current_symbol = roman_dict[roman_string[i]]
-        if i == len(roman_string) - 1 or current_symbol >= roman_dict[roman_string[i + 1]]:
-            result += current_symbol
+        cur_symbol = roman_dict[roman_string[i]]
+        if i == len(roman_string) - 1:
+            result += cur_symbol
+        elif cur_symbol >= r_dict[roman_string[i + 1]]:
+            result += cur_symbol
         else:
-            result -= current_symbol
+            result -= cur_symbol
     return result
