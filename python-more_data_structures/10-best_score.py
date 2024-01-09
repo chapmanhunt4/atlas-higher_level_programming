5ec7803c9f3b39a3aae4cd377989aef560aa3751
chapmanhunt4 <chapmanhunt4@gmail.com>
2024-01-08 16:27:39 -0800

    had to add the key= even though I got proper output locally

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -3,5 +3,5 @@ def best_score(a_dictionary):
     if a_dictionary is None or len(a_dictionary) == 0:
         return None
     else:
-        top_score = max(a_dictionary)
+        top_score = max(a_dictionary, key=a_dictionary.get)
         return top_score
