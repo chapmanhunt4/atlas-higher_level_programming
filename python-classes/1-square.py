75010c380e4472313a5fca50c57d663172ba2122
chapmanhunt4 <chapmanhunt4@gmail.com>
2024-01-12 11:17:57 -0800

    made Square class with a private size attribute

---
+++ python-classes/1-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""
+This module defines a class Square with an attribute size.
+"""
+
+
+class Square:
+    """
+    The Square class with a size attribute.
+    """
+    def __init__(self, size):
+        """
+        The constructor for the Square class.
+
+        Parameters:
+        size (int): The size of the square.
+        """
+        self.__size = size
